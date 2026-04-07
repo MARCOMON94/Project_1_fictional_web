d1a251111ecab7d553dc2fc7dd28968d6cb6059a
MARCO <jcmm.1994@gmail.com>
2026-04-07 12:05:05 +0100

    Web base

---
+++ js/main.js
@@ -0,0 +1,43 @@
+window.addEventListener("DOMContentLoaded", () => {
+  loadRecentProjects();
+});
+
+function loadRecentProjects() {
+  fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("No se pudo obtener la información de los proyectos");
+      }
+      return response.json();
+    })
+    .then((projects) => {
+  const orderedProjects = projects.sort((a, b) => Number(a.uuid) - Number(b.uuid));
+  const recentProjects = orderedProjects.slice(0, 3);
+  renderProjects(recentProjects);
+})
+    .catch((error) => {
+      console.error("Error al cargar los proyectos:", error);
+
+      const container = document.getElementById("projects-container");
+      container.innerHTML = `<p>No se pudieron cargar los proyectos.</p>`;
+    });
+}
+
+function renderProjects(projects) {
+  const container = document.getElementById("projects-container");
+
+  container.innerHTML = "";
+
+  projects.forEach((project) => {
+    container.innerHTML += `
+      <article class="project-card">
+        <img src="${project.image}" alt="${project.name}">
+        <div class="project-card-content">
+          <h3>${project.name}</h3>
+          <p>${project.description}</p>
+          <a href="./projects/1.html">Learn More</a>
+        </div>
+      </article>
+    `;
+  });
+}
